aaf698c9f755c71b739117478ef47a853645b806
mimot <medina.manolitojr@gmail.com>
2025-03-04 15:28:35 +0800

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,22 @@
+--Create new database "DataWarehouse" and connect to it
+USE master;
+IF DB_ID('DataWarehouse') IS NOT NULL
+BEGIN
+	DROP DATABASE DataWarehouse;
+END 
+BEGIN
+	CREATE DATABASE DataWarehouse;
+END 
 
+USE DataWarehouse;
+
+--Create schemas
+
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+GO
+
+--
